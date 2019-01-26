f822fbcbdf01ebdf1f947547c2387e937f41716a
GoodKenneth <kenthesage@gmail.com>
2019-01-25 22:09:40 -0500

    Add TilemapGridObject

--- Assets/Primitives/Grids/GridObject.cs
+++ Assets/Primitives/Grids/GridObject.cs
@@ -12,6 +12,8 @@
             public List<Vector3Int> volume = new List<Vector3Int> { Vector3Int.zero };
             public GameGrid grid;
 
+            public virtual void RemapVolume(List<Vector3Int> vol) {}
+
             public void RegisterObject(GameGrid g) {
                 if (g.IsColliding(this) == true)
                     return;
---
+++ Assets/Scripts/TilemapGridObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+using Primitives.Grid;
+
+public class TilemapGridObject : GridObject
+{
+    public Tilemap tilemap;
+    public Tile tile;
+
+    void Start()
+    {
+        if (tilemap != null) {
+            RemapVolume(volume);
+        }
+    }
+
+    public virtual void RemapVolume(List<Vector3Int> vol) {
+        tilemap.ClearAllTiles();
+
+        foreach (Vector3Int p in vol) {
+            tilemap.SetTile(p, tile);
+        }
+    }
+}
